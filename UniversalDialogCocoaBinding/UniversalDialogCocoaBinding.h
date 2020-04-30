65823b8e52a25a7ad63e8e12cec7af0481c943d8
mahoshojoHCG <yzy688@gmail.com>
2020-04-30 17:08:13 +0800

    Initial Commit

---
+++ UniversalDialogCocoaBinding/UniversalDialogCocoaBinding.h
@@ -0,0 +1,13 @@
+//
+//  UniversalDialogCocoaBinding.h
+//  UniversalDialogCocoaBinding
+//
+//  Created by 尹照宇 on 2020/4/30.
+//  Copyright © 2020 尹照宇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UniversalDialogCocoaBinding : NSObject
+
+@end
---
+++ UniversalDialogCocoaBinding/UniversalDialogCocoaBinding.m
@@ -0,0 +1,13 @@
+//
+//  UniversalDialogCocoaBinding.m
+//  UniversalDialogCocoaBinding
+//
+//  Created by 尹照宇 on 2020/4/30.
+//  Copyright © 2020 尹照宇. All rights reserved.
+//
+
+#import "UniversalDialogCocoaBinding.h"
+
+@implementation UniversalDialogCocoaBinding
+
+@end
