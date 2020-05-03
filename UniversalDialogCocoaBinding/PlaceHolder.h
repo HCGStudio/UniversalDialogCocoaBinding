56cb1fbae773645008aeadca72942219b310de26
mahoshojoHCG <yzy688@gmail.com>
2020-05-03 12:48:48 +0800

    Add files.

---
+++ UniversalDialogCocoaBinding/PlaceHolder.h
@@ -0,0 +1,11 @@
+//This file do nothing but make C++ source linkable to CFoundition.
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaceHolder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UniversalDialogCocoaBinding/PlaceHolder.m
@@ -0,0 +1,7 @@
+//This file do nothing but make C++ source linkable to CFoundition.
+
+#import "PlaceHolder.h"
+
+@implementation PlaceHolder
+
+@end
